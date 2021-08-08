e01414a0caee6ee7cda2c34b0ff52dbaf425eeac
Seunghwanly <seunghwanly@gmail.com>
2021-08-09 01:16:34 +0900

    :bug: sth went wrong while building build_runner

--- woomam/lib/model/store.g.dart
+++ woomam/lib/model/store.g.dart
@@ -5,3 +5,19 @@ part of 'store.dart';
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
+
+Store _$StoreFromJson(Map<String, dynamic> json) => Store(
+      writtenDate: DateTime.parse(json['writtenDate'] as String),
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      storeName: json['storeName'] as String,
+      storeUID: json['storeUID'] as String,
+    );
+
+Map<String, dynamic> _$StoreToJson(Store instance) => <String, dynamic>{
+      'storeUID': instance.storeUID,
+      'storeName': instance.storeName,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+      'writtenDate': instance.writtenDate.toIso8601String(),
+    };
